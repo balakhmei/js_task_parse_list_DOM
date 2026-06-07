9f6b79f0bde6f3bfc704079d332f1f314a05fcb9
Ihor Balakhmei <ihor@MacBook-Pro-Ihor.local>
2026-06-07 21:47:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function sortList(listElement) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  function convertSalary(salaryString) {
+    return Number(salaryString.replace('$', '').replace(',', ''));
+  }
+
+  const sortSalary = items.sort(
+    (a, b) => convertSalary(b.dataset.salary) - convertSalary(a.dataset.salary),
+  );
+
+  return list.append(...sortSalary);
+}
+
+function getEmployees(listElement) {
+  const items = Array.from(list.querySelectorAll('li'));
+
+  const employee = items.map((li) => {
+    return {
+      name: li.dataset.name,
+      position: li.dataset.position,
+      salary: Number(li.dataset.salary),
+      age: li.dataset.age,
+    };
+  });
+
+  return employee;
+}
+
+const list = document.querySelector('ul');
+
+sortList(list);
+getEmployees(list);
